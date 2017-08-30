c7109ff529eeec22a84362e914abe69b0b50fe71
github username <github email address>
2017-08-30 16:43:55 -0400

    Version 2.0

--- lib/person.rb
+++ lib/person.rb
@@ -15,13 +15,10 @@ class Person
   end
 
   def happiness=(happiness)
-    #Sets happiness and sets happiness limits
-      @happiness = happiness
-    if @happiness < 0
-        @happiness = 0
-    elsif @happiness > 10
-        @happiness = 10
-    end 
+    #Sets happiness and min/max
+    @happiness = happiness
+    @happiness = 0 if @happiness < 0
+    @happiness = 10 if @happiness > 10
   end
 
   def bank_account=(bank_account)
@@ -30,8 +27,10 @@ class Person
   end
 
   def hygiene=(hygiene)
-    #sets_hygiene
+    #sets_hygiene and min/max
     @hygiene = hygiene
+    @hygiene = 0 if @hygiene < 0
+    @hygiene = 10 if @hygiene > 10
   end
 
   def happiness
@@ -48,5 +47,23 @@ class Person
     #gets hygiene
     @hygiene
   end
+
+  def happy?
+    @happiness > 7 ? true : false
+  end
+
+  def clean?
+    @hygiene > 7 ? true : false
+  end
+
+  def get_paid(salary)
+    @bank_account += salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    hygiene=(@hygiene += 4)
+  end
+
 binding.pry
 end
