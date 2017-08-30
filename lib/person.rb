1394d052ffb69be7f8071ab753c43905f2fb29ad
github username <github email address>
2017-08-30 16:21:52 -0400

    Version 1.0

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,52 @@
 # your code goes here
+require "pry"
+
+class Person
+  #create the person class
+  # attr_accessor :bank_account, :happiness, :hygiene
+
+  def initialize(name)
+    # inti with name, bank_account, happiness_points, hygiene_points
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+
+  end
+
+  def happiness=(happiness)
+    #Sets happiness and sets happiness limits
+      @happiness = happiness
+    if @happiness < 0
+        @happiness = 0
+    elsif @happiness > 10
+        @happiness = 10
+    end 
+  end
+
+  def bank_account=(bank_account)
+    #sets bank_account
+    @bank_account = bank_account
+  end
+
+  def hygiene=(hygiene)
+    #sets_hygiene
+    @hygiene = hygiene
+  end
+
+  def happiness
+    #gets happiness
+    @happiness
+  end
+
+  def bank_account
+    #gets bank_account
+    @bank_account
+  end
+
+  def hygiene
+    #gets hygiene
+    @hygiene
+  end
+binding.pry
+end
