3f71e0b3bc5aa43522e915eca2e779b956d547c8
github username <github email address>
2017-08-30 18:00:56 -0400

    Version Final

--- lib/person.rb
+++ lib/person.rb
@@ -66,22 +66,35 @@ class Person
   end
 
   def take_bath
-    self.hygiene=(@hygiene+=4)
+    self.hygiene=(hygiene + 4)
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
   def work_out
-    self.hygiene=(@hygiene -= 3)
-    self.happiness=(@happiness += 2)
+    self.hygiene=(hygiene - 3)
+    self.happiness=(happiness + 2)
     "♪ another one bites the dust ♫"
   end
 
   def call_friend(person)
-    self.happiness=(@happiness + 3)
+    self.happiness=(happiness + 3)
     person.happiness=(person.happiness + 3)
     "Hi #{person.name}! It's #{self.name}. How are you?"
   end
 
+  def start_conversation(person,topic)
+    if topic == "politics"
+      self.happiness=(happiness - 2)
+      person.happiness=(person.happiness - 2)
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+        self.happiness=(happiness + 1)
+        person.happiness=(person.happiness + 1)
+        "blah blah sun blah rain"
+      else
+        "blah blah blah blah blah"
+  end
+end
 
 binding.pry
 end
