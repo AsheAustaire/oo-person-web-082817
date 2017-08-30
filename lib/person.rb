7fd8a96b5be12028d6fd5a8a13a49412db805ac7
github username <github email address>
2017-08-30 17:16:28 -0400

    Version 3.0

--- lib/person.rb
+++ lib/person.rb
@@ -62,7 +62,14 @@ class Person
   end
 
   def take_bath
-    hygiene=(@hygiene += 4)
+    self.hygiene=(@hygiene+=4)
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene=(@hygiene -= 3)
+    self.happiness=(@happiness += 2)
+    "♪ another one bites the dust ♫"
   end
 
 binding.pry
